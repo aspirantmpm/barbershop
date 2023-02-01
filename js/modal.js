3642bf48906e6ab181f6b66e819d930a5c396883
Vadym <aspirantmpm@gmail.com>
2023-02-01 15:21:23 +0200

    buttons

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,17 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const openModalBtn = document.querySelectorAll('[data-modal-open]');
+const closeModalBtn = document.querySelector('[data-modal-close]');
+const modal = document.querySelector('[data-modal]');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  console.log(openModalBtn);
+  // openModalBtn.addEventListener('click', toggleModal);
+  closeModalBtn.addEventListener('click', toggleModal);
+
+for (var i = 0; i < openModalBtn.length; i++) {
+  openModalBtn[i].addEventListener('click', toggleModal);
+}
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    modal.classList.toggle('is-hidden');
   }
-})();
+
